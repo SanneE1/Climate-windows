af02dfd3ee4accb06d83da80ca55124044a716cc
SanneE1 <sanne.m.evers@gmail.com>
2019-09-12 19:35:38 +0200

    added notes for future steps to make it generic

--- Analysis/Climwin/Merge_arrays.R
+++ Analysis/Climwin/Merge_arrays.R
@@ -1,9 +1,8 @@
-library(climwin)
-library(dplyr)
-library(lme4)
 
+library(dplyr)
 
 ## get all RDS from one job in one list  --------------------------------------------------------------------------------------------------
+## To make it generic, figure out how to use output destination, jobID etc.
 
 files <- list.files("Results/Climwin/Slidingwin/", pattern = "^Surv_Climwin_HEQU_month1_5528243_.*\\.rds$", full.names = T)
 r <- lapply(files, readRDS)
@@ -15,5 +14,7 @@ r <- lapply(files, readRDS)
 results <- lapply(r, '[[', 1)
 results$combos <- bind_rows(lapply(r, '[[', 2), .id="column_label")
 
+
+## See if this can then also be more generic - this would then be named same as input, but withour taksID
 saveRDS(results, "Results/Climwin/Slidingwin/Surv_Climwin_HEQU_month1_5528243.rds")
 
