be8c7f3448bbef89fdfb789483b50aa155b24824
SanneE1 <sanne.m.evers@gmail.com>
2019-09-17 15:05:11 +0200

    fixed error for adding directory per job

--- Analysis/Climwin/Survival_Climwin_submit.sh
+++ Analysis/Climwin/Survival_Climwin_submit.sh
@@ -23,13 +23,18 @@
 #needed when submitting a non-parallel job
 #$ -binding linear:1
 
+#create a single output directory per job
+output_dir="/work/$USER/$JOB_NAME-$JOB_ID"
+mkdir -p "$output_dir"
+
+
 module load foss/2018b R/3.5.1
 
 climate=$1
 SpeciesInput=$2
 cdate=$(basename $climate .csv | cut -d _ -f3)
 species=$(basename $SpeciesInput .csv | cut -d _ -f1)
-output=/work/$USER/$JOB_NAME-$JOB_ID/${JOB_NAME}_${species}_${cdate}_${JOB_ID}_$SGE_TASK_ID.rds
+output="$output_dir"/${JOB_NAME}_${species}_${cdate}_${JOB_ID}_$SGE_TASK_ID.rds
 
 
 Rscript $HOME/Biome/Analysis/Climwin/Survival_Climwin.R \
