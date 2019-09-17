ee3651c70cccb81914de1bf66e14d9d2130af913
SanneE1 <sanne.m.evers@gmail.com>
2019-09-17 13:51:19 +0200

    Also save the combos table

--- Analysis/Climwin/Merge_arrays.R
+++ Analysis/Climwin/Merge_arrays.R
@@ -1,21 +1,26 @@
 
 library(dplyr)
 
+## Set directory at the output path of the first job ----------------------------------------------------------------------------------------------
+
 output_dir <- commandArgs(trailingOnly = T)[1]
-## get all RDS from one job in one list  --------------------------------------------------------------------------------------------------
+
+## get all RDS from one job in one list  ----------------------------------------------------------------------------------------------------------
 ## To make it generic, figure out how to use output destination, jobID etc.
 
 files <- list.files(output_dir, pattern = "^Surv_Climwin.*\\.rds$", full.names = T)
 r <- lapply(files, readRDS)
 
 
-## Create the correct result format --------------------------------------------------------------------------------------------------------
+## Create the correct result format ---------------------------------------------------------------------------------------------------------------
 
 
 results <- lapply(r, '[[', 1)
 results$combos <- bind_rows(lapply(r, '[[', 2), .id="column_label")
 
 
-## See if this can then also be more generic - this would then be named same as input, but withour taksID
+##Save files in the directory created by the Climwin function -------------------------------------------------------------------------------------
+
 saveRDS(results, file.path(output_dir, "result.rds"))
+write.csv(results$combos, file.path(output_dir, "summary_combos.csv") )
 
