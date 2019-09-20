583f6ba94056e8712fc8b82974db5633c7228afd
Sanne Evers <sanne.evers@idiv.de>
2019-09-20 17:32:37 +0200

    Modified Growth_Baseline R and created submitscript

--- Analysis/Climwin/HEQU_Growth_Baseline.R
+++ Analysis/Climwin/HEQU_Growth_Baseline.R
@@ -1,21 +1,9 @@
-## R code to run the sliding window on the Helianthella quinquenervis dataset 
-
-setwd("c:/owncloud/Documents/PhD/Biomes/Biome/")
-
-library(climwin)
 library(dplyr)
 library(lme4)
 
 ### Prepare data -------------------------------------------------------------
-Clim <- read.csv("Data/Climate data/HEQU_NOAA_month.csv") 
 
-# Clim$date <- as.Date(Clim$date)                                    ### DAILY data
-# Clim$date <- format(Clim$date, format = "%d/%m/%Y")                ### DAILY data
-
-Clim$date <- paste("15/",sprintf("%02d", Clim$Month), "/", Clim$Year, sep = "")  ### MONTHLY data         
-
-
-Biol <- read.csv("Data/Biol data/HEQU_demography_data.csv") %>%
+Biol <- read.csv("/data/gsclim/HEQU_demography_data.csv") %>%
   mutate(sizeT = as.numeric(as.character(sizeT)),
          sizeT1 = as.numeric(as.character(sizeT1)),
          year = as.factor(year))
