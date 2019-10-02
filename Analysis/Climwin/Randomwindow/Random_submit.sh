3bfb9c4dceb1cc3d686132f552ef5733feff0609
Sanne Evers <sanne.evers@idiv.de>
2019-10-02 15:03:24 +0200

    Correcting random output name 2.0

--- Analysis/Climwin/Randomwindow/Random_submit.sh
+++ Analysis/Climwin/Randomwindow/Random_submit.sh
@@ -35,7 +35,7 @@ Results_sliding=$3
 winner=$4
 cdata=$(basename $Climate .csv | cut -d _ -f3)
 species=$(basename $SpeciesInput .csv | cut -d _ -f1)
-output="$output_dir"/${JOB_NAME}_${species}_${cdate}_${JOB_ID}_$SGE_TASK_ID.rds
+output="$output_dir"/${JOB_NAME}_${species}_${cdata}_${JOB_ID}_$SGE_TASK_ID.rds
 
  
 Rscript $HOME/Biome/Analysis/Climwin/Randomwindow/Random.R \
