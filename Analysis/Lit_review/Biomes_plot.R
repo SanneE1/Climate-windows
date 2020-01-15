ee0fe4d29f391d18ee140d36c9695f8a7d0311a7
Sanne Evers <sanne.evers@idiv.de>
2020-01-15 15:26:01 +0100

    Restructuring for official lit review

---
+++ Analysis/Lit_review/Biomes_plot.R
@@ -0,0 +1,153 @@
+## Create plots for an overview of populations in the plant revies (Compagnoni et al., in preperation)
+##
+
+library(dplyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(scales)
+library(patchwork)
+
+data <- readxl::read_excel("Analysis/Lit_review/Biomes from sApropos.xlsx",sheet = "Sheet1", skip = 1)
+data <- data[!is.na(data$Biome),]
+
+
+### Divide into Regions --------------------------------------------------------------------------------------
+
+Tropical <- c("TMB", "TDB", "TSC", "TGV", "MAN")
+Temperate <-  c("FGS", "TBM", "TGS", "TCF", "MED")
+Alpine <- c("MON")
+Tundra <- c("TUN", "BOR")
+Boreal <- c("BOR")
+Arid <- c("DES")
+
+data$region <- 1
+data$region[which(data$Biome %in% Tropical)] <- "Tropical & Subtropical"
+data$region[which(data$Biome %in% Temperate)] <- "Temperate"
+data$region[which(data$Biome %in% Alpine)] <- "Alpine"
+data$region[which(data$Biome %in% Tundra)] <- "Tundra & Boreal Forest"
+#data$region[which(data$Biome %in% Boreal)] <- "Boreal Forest"
+data$region[which(data$Biome %in% Arid)] <- "Arid"
+data$region <- factor(data$region, levels = c("Arid", "Tundra & Boreal Forest", "Temperate", "Tropical & Subtropical"))
+
+
+## Prepare dataframes ---------------------------------------------------------------------------------------------
+
+df <- data %>%
+  group_by(region) %>%
+  summarise(Populations = length(unique(c(plant_species, Population))))
+             
+
+df1 <- data %>%
+  group_by(region, TType) %>%
+  summarise(Populations = length(unique(c(plant_species, Population)))) %>%
+  na.omit(TType)
+
+df1$total2 <- 1
+df1$total2[which(df1$region=="Arid")] = sum(df1$Populations[which(df1$region == "Arid")])
+df1$total2[which(df1$region=="Tundra & Boreal Forest")] = sum(df1$Populations[which(df1$region == "Tundra & Boreal Forest")])
+df1$total2[which(df1$region=="Temperate")] = sum(df1$Populations[which(df1$region == "Temperate")])
+df1$total2[which(df1$region=="Tropical & Subtropical")] = sum(df1$Populations[which(df1$region == "Tropical & Subtropical")])
+df1$relative2 <- df1$Populations/df1$total2
+df1$TType <- factor(df1$TType, levels = c("Both", "Seasonal", "Annual"))
+
+df2 <- data %>%
+  group_by(region, Ptype) %>%
+  summarise(Populations = length(unique(c(plant_species, Population)))) %>%
+  na.omit(Ptype)
+
+df2$total2 <- 1
+df2$total2[which(df2$region=="Arid")] = sum(df2$Populations[which(df2$region == "Arid")])
+df2$total2[which(df2$region=="Tundra & Boreal Forest")] = sum(df2$Populations[which(df2$region == "Tundra & Boreal Forest")])
+df2$total2[which(df2$region=="Temperate")] = sum(df2$Populations[which(df2$region == "Temperate")])
+df2$total2[which(df2$region=="Tropical & Subtropical")] = sum(df2$Populations[which(df2$region == "Tropical & Subtropical")])
+df2$relative2 <- df2$Populations/df2$total2
+
+df2$Ptype <- factor(df2$Ptype, levels = c("Both", "Seasonal", "Annual"))
+
+### Include those studies that don't considere the specific driver  ------------------------------------------------------------------------------------
+# df <- data %>%
+#   group_by(region) %>%
+#   summarise(Populations = length(unique(c(plant_species, Population))))
+# 
+# 
+# df1 <- data %>%
+#   group_by(region, TType) %>%
+#   summarise(Populations = length(unique(c(plant_species, Population)))) 
+# 
+# df1$TType[which(is.na(df1$TType))] <- "None"
+# df1$total2 <- 1
+# df1$total2[which(df1$region=="Arid")] = sum(df1$Populations[which(df1$region == "Arid")])
+# df1$total2[which(df1$region=="Tundra and Boreal Forest")] = sum(df1$Populations[which(df1$region == "Tundra and Boreal Forest")])
+# df1$total2[which(df1$region=="Temperate")] = sum(df1$Populations[which(df1$region == "Temperate")])
+# df1$total2[which(df1$region=="Tropical & Subtropical")] = sum(df1$Populations[which(df1$region == "Tropical & Subtropical")])
+# df1$relative2 <- df1$Populations/df1$total2
+# df1$TType <- factor(df1$TType, levels = c("None", "Both", "Seasonal", "Annual"))
+# 
+# df2 <- data %>%
+#   group_by(region, Ptype) %>%
+#   summarise(Populations = length(unique(c(plant_species, Population)))) 
+# 
+# df2$Ptype[which(is.na(df2$Ptype))] <- "None"
+# df2$total2 <- 1
+# df2$total2[which(df2$region=="Arid")] = sum(df2$Populations[which(df2$region == "Arid")])
+# df2$total2[which(df2$region=="Tundra and Boreal Forest")] = sum(df2$Populations[which(df2$region == "Tundra and Boreal Forest")])
+# df2$total2[which(df2$region=="Temperate")] = sum(df2$Populations[which(df2$region == "Temperate")])
+# df2$total2[which(df2$region=="Tropical & Subtropical")] = sum(df2$Populations[which(df2$region == "Tropical & Subtropical")])
+# df2$relative2 <- df2$Populations/df2$total2
+# 
+# df2$Ptype <- factor(df2$Ptype, levels = c("None", "Both", "Seasonal", "Annual"))
+# 
+
+### Absolute population numbers ------------------------------------------------------------------------------------
+
+byBiome <- ggplot(data = df, aes(x=region, y=Populations)) +
+  geom_bar(stat = "identity", fill = "#009E73") +
+  labs(fill = "Biomes") +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.y = element_text(size = 15),
+        axis.title.x = element_blank(),
+        axis.line.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey80"))
+
+
+### Relative bargraphs ----------------------------------------------------------------------------------------------------
+
+CTemp <- ggplot(data = df1, aes(x = region , y= relative2, fill = TType, label = Populations )) +
+  geom_bar(stat = "identity") +
+  geom_text(size = 4, position = position_stack(vjust = 0.5)) +
+  scale_x_discrete(name = element_blank(), labels = c("Arid", "Tundra & \nBoreal Forest", "Temperate", "Tropical & \n Subtropical")) +
+  scale_y_continuous(labels=scales::percent_format(accuracy = 1), name = element_blank())+
+  labs(fill = "Temperature \ndriver", title = "Populations considering temperature")+
+  scale_fill_brewer(palette = "OrRd" ) +
+  theme_classic()+
+  theme(axis.line.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey80"))
+
+CPrcp <- ggplot(data = df2, aes(x = region , y= relative2, fill = Ptype, label = Populations )) +
+  geom_bar(stat = "identity") +
+  geom_text(size = 4, position = position_stack(vjust = 0.5))+
+  scale_x_discrete(name = element_blank(), labels = c("Arid", "Tundra & \nBoreal Forest", "Temperate", "Tropical & \n Subtropical")) +
+  scale_y_continuous(labels=scales::percent_format(accuracy = 1), name = element_blank())+
+  labs(fill = "Precipitation \ndriver", title = "Populations considering precipitation") +
+  scale_fill_brewer(palette = "Blues" ) +
+  theme_classic() +
+  theme(axis.line.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey80"))
+
+
+### Arrange all options --------------------------------------------------------------------------------
+
+All <- byBiome / (CTemp / CPrcp)  + 
+  plot_layout(heights = c(1,2)) + 
+  plot_annotation(tag_levels = c('A'))
+
+
+### Save plots ----------------------------------------------------------------------------------------
+
+ggsave("Visual/Biomeplot.png", All, width = 8, height = 9)
+
---
+++ Analysis/Lit_review/Windows.R
@@ -0,0 +1,68 @@
+library(dplyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(patchwork)
+
+
+### Get data --------------------------------------------------------------------------------------------------------
+
+df <- readxl::read_excel("Analysis/Lit_review/Biomes window open close.xlsx", skip = 1) %>%
+  filter(Biome == "DES")
+df$Studies <- as.factor(df$Studies)
+df$TP <- as.factor(df$`T/P`)
+df <- df[which(df$Studies != "Maschinski et al. 1997"),]     ## Remove studies with missing information
+df <- df[which(df$Studies != "Raghu et al. 2013"),]
+
+df1 <- readxl::read_excel("Analysis/Lit_review/Biomes window open close.xlsx", skip = 1) %>%
+  filter(Biome == "TCF")
+df1$Studies <- as.factor(df1$Studies)
+df1$TP <- as.factor(df1$`T/P`)
+df1 <- df1[which(df1$Studies != "Stevens&Latimer 2015"),]     ## Remove studies with missing information
+df1 <- df1[which(df1$Studies != "Olsen et al. 2016"),]
+df1 <- df1[which(df1$Studies != "Conlisk et al. 2017"),]
+
+### Create graphs --------------------------------------------------------------------------------------------------------
+
+Arid <- ggplot(df, aes(ymin = df$Closes, ymax = df$Open, x = df$Studies, colour = df$TP, group = df$TP)) +
+  geom_point(aes(y=df$Open, x = df$Studies), shape = 4, position = position_dodge(width = 0.4), size = 1, stroke = 2)+
+  geom_point(aes(y=df$Closes, x = df$Studies), shape = 3, position = position_dodge(width = 0.4), size = 1, stroke = 2)+
+  geom_linerange(aes(ymin = df$Closes, ymax = df$Open, x = df$Studies), size = 1.25 ,position = position_dodge(width = 0.4)) +
+  coord_flip()+
+  ggtitle("Arid") +
+  scale_y_continuous(breaks = c(0,6,12,18,24), 
+                     name = "Months relative to observation at month 0") +
+  scale_colour_manual(name = "Driver", 
+                      labels = c('Precipitation', 'Temperature'),
+                      values = c('P' = '#0072B2', 'T' = '#D55E00'),
+                      breaks = c('P', 'T')) +
+  theme_minimal() +
+  theme(axis.title = element_blank(),
+        plot.title = element_text(hjust = -0.3))
+
+
+TCF <- ggplot(df1, aes(ymin = df1$Closes, ymax = df1$Open, x = df1$Studies, colour = df1$TP, group = df1$TP)) +
+  geom_point(aes(y=df1$Open, x = df1$Studies), shape = 4, position = position_dodge(width = 0.4), size = 1, stroke = 2)+
+  geom_point(aes(y=df1$Closes, x = df1$Studies), shape = 3, position = position_dodge(width = 0.4), size = 1, stroke = 2)+
+  geom_linerange(aes(ymin = df1$Closes, ymax = df1$Open, x = df1$Studies), size = 1.25 ,position = position_dodge(width = 0.4)) +
+  coord_flip()+
+  ggtitle("TCF") +
+  scale_y_continuous(breaks = c(0,6,12,18,24), 
+                     name = "Months relative to observation at month 0") +
+  scale_colour_manual(name = "Driver", 
+                      labels = c('Precipitation', 'Temperature'),
+                      values = c('P' = '#0072B2', 'T' = '#D55E00'),
+                      breaks = c('P', 'T')) +
+  theme_minimal() +
+  theme(axis.title.y = element_blank(),
+        plot.title = element_text(hjust = -0.3))
+
+
+
+
+both <- (Arid / TCF) + plot_layout(guides = "collect", heights = c(4,2))
+
+# ggsave("Visual/Time windows Arid.png", Arid)
+# ggsave("Visual/Time windows TCF.png", TCF)
+ggsave("Visual/Time windows Arid&TCF.png", both, width = 10, height = 5)
+
