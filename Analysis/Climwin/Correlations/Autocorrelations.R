5c22eacff6e9fb1f9e9cf6c938d65dbffb207bc2
Sanne <sanne.evers@idiv.de>
2020-04-15 15:46:59 +0200

    Add correlations (previously only autocorrelations)

--- Analysis/Climwin/Correlations/Autocorrelations.R
+++ Analysis/Climwin/Correlations/Autocorrelations.R
@@ -9,136 +9,128 @@ source("Analysis/Climwin/Correlations/required_functions.R")
 
 ### Survival
 
-AutoHs <- autocor_wrap(species = "HEQU", 
+AutoHs <- cor_wrap(species = "HEQU", 
                        vitalrate = "s", 
                        Climate = "data/Climate data/HEQU_NOAA_month_imputed.csv", 
                        SlidingObject = Hs, 
                        Winner = Hsurv)
 
-AutoFs <- autocor_wrap(species = "FRSP", 
+AutoFs <- cor_wrap(species = "FRSP", 
                       vitalrate = "s", 
                       Climate = 'data/Climate data/FRSP_NOAA_month.csv', 
                       SlidingObject = Fs, 
                       Winner = Fsurv)
 
-AutoCs <- autocor_wrap(species = "CRFL", 
+AutoCs <- cor_wrap(species = "CRFL", 
                        vitalrate = "s", 
                        Climate = "data/Climate data/CRFL_NOAA_month.csv", 
                        SlidingObject = Cs,
                        Winner = Csurv)
 
-AutoOs <- autocor_wrap("OPIM", 
+AutoOs <- cor_wrap("OPIM", 
                        "s", 
                        "data/Climate data/OPIM_SEVLTER_month_imputed.csv", 
                        Os, 
                        Osurv)
-a <- acorplot(AutoHs) + ggtitle("Autocorrelation of Tobs \nwith circled window", subtitle = "In relation to H. quinquenervis survival")
-b <- acorplot(AutoFs) + ggtitle("Autocorrelation of T_mean_max \nwith circled window", subtitle = "In relation to F. speciosa survival")
-c <- acorplot(AutoCs) + ggtitle("Autocorrelation of Tmax \nwith circled window", subtitle = "In relation to C. flava survival")
-d <- acorplot(AutoOs) + ggtitle("Autocorrelation of Tavg \nwith circled window", subtitle = "In relation to C. imbricata survival")
 
-AutoSurv <- (a+b) / (c+d) + plot_layout(guides = "collect")
-ggsave("Visual/Autocorrelation_survival.png", AutoSurv)
 
 ### Growth
 
-AutoHg <- autocor_wrap("HEQU", 
+AutoHg <- cor_wrap("HEQU", 
                        "g", 
                        "data/Climate data/HEQU_NOAA_month_imputed.csv", 
                        Hg, 
                        Hgrowth)
 
-AutoFg <- autocor_wrap("FRSP", 
+AutoFg <- cor_wrap("FRSP", 
                        "g", 
                        "data/Climate data/FRSP_NOAA_month.csv", 
                        Fg, 
                        Fgrowth)
 
-AutoCg <- autocor_wrap("CRFL", 
+AutoCg <- cor_wrap("CRFL", 
                        "g", 
                        "data/Climate data/CRFL_NOAA_month.csv", 
                        Cg,
                        Cgrowth)
 
-AutoOg <- autocor_wrap("OPIM", 
+AutoOg <- cor_wrap("OPIM", 
                        "g", 
                        "data/Climate data/OPIM_SEVLTER_month_imputed.csv", 
                        Og, 
                        Ogrowth)
 
-e <- acorplot(AutoHg) + ggtitle("Autocorrelation of Tmean_min \nwith circled window")
-f <- acorplot(AutoFg) + ggtitle("Autocorrelation of Pmean \nwith circled window")
-g <- acorplot(AutoCg) + ggtitle("Autocorrelation of Pmean \nwith circled window")
-h <- acorplot(AutoOg) + ggtitle("Autocorrelation of Tmean_max \nwith circled window")
-
-Autogrowth <- (e+f)/(g+h) + plot_layout(guides = "collect")
-ggsave("Visual/Autocorrelation_growth.png", Autogrowth)
-
 
 ### Flower Prob
 
-AutoHfp <- autocor_wrap("HEQU", 
+AutoHfp <- cor_wrap("HEQU", 
                        "fp", 
                        "data/Climate data/HEQU_NOAA_month_imputed.csv", 
                        Hfp, 
                        HpFlwr)
 
-AutoFfp <- autocor_wrap("FRSP", 
+AutoFfp <- cor_wrap("FRSP", 
                        "fp", 
                        "data/Climate data/FRSP_NOAA_month.csv", 
                        Ffp, 
                        FpFlwr)
 
-AutoCfp <- autocor_wrap("CRFL", 
+AutoCfp <- cor_wrap("CRFL", 
                        "fp", 
                        "data/Climate data/CRFL_NOAA_month.csv", 
                        Cfp,
                        CpFlwr)
 
-AutoOfp <- autocor_wrap("OPIM", 
+AutoOfp <- cor_wrap("OPIM", 
                        "fp", 
                        "data/Climate data/OPIM_SEVLTER_month_imputed.csv", 
                        Ofp, 
                        OpFlwr)
 
-i <- acorplot(AutoHfp) + ggtitle("Autocorrelation of Tmax \nwith circled window")
-j <- acorplot(AutoFfp) + ggtitle("Autocorrelation of Tmax \nwith circled window")
-k <- acorplot(AutoCfp) + ggtitle("Autocorrelation of SPEI \nwith circled window")
-l <- acorplot(AutoOfp) + ggtitle("Autocorrelation of T_mean_min \nwith circled window")
-
-AutoFp <- (i+j) / (k+l) + plot_layout(guides = "collect")
-ggsave("Visual/Autocorrelation_pFlwr.png", AutoFp)
 
 ### Flower Numbers
 
-AutoHfn <- autocor_wrap("HEQU", 
+AutoHfn <- cor_wrap("HEQU", 
                         "fn", 
                         "data/Climate data/HEQU_NOAA_month_imputed.csv", 
                         Hfn, 
                         HnFlwr)
 
-AutoFfn <- autocor_wrap("FRSP", 
+AutoFfn <- cor_wrap("FRSP", 
                         "fn", 
                         "data/Climate data/FRSP_NOAA_month.csv", 
                         Ffn, 
                         FnFlwr)
 
-AutoCfn <- autocor_wrap("CRFL", 
+AutoCfn <- cor_wrap("CRFL", 
                         "fn", 
                         "data/Climate data/CRFL_NOAA_month.csv", 
                         Cfn,
                         CnFlwr)
 
-AutoOfn <- autocor_wrap("OPIM", 
+AutoOfn <- cor_wrap("OPIM", 
                         "fp", 
                         "data/Climate data/OPIM_SEVLTER_month_imputed.csv", 
                         Ofn, 
                         OnFlwr)
 
-m <- acorplot(AutoHfn) + ggtitle("Autocorrelation of Pmean \nwith circled window")
-n <- acorplot(AutoFfn) + ggtitle("Autocorrelation of Tmean_min \nwith circled window")
-o <- acorplot(AutoCfn) + ggtitle("Autocorrelation of SPEI \nwith circled window")
-p <- acorplot(AutoOfn) + ggtitle("Autocorrelation of Tavg \nwith circled window")
 
-AutoFn <- (m+n) / (o+p) + plot_layout(guides = "collect")
-ggsave("Visual/Autocorrelation_nFlwr.png", AutoFn)
+ggsave("Visual/Correlation_Hs.png", AutoHs)
+ggsave("Visual/Correlation_Hg.png", AutoHg)
+ggsave("Visual/Correlation_Hfp.png", AutoHfp)
+ggsave("Visual/Correlation_Hfn.png", AutoHfn)
+
+ggsave("Visual/Correlation_Fs.png", AutoFs)
+ggsave("Visual/Correlation_Fg.png", AutoFg)
+ggsave("Visual/Correlation_Ffp.png", AutoFfp)
+ggsave("Visual/Correlation_Ffn.png", AutoFfn)
+
+ggsave("Visual/Correlation_Os.png", AutoOs)
+ggsave("Visual/Correlation_Og.png", AutoOg)
+ggsave("Visual/Correlation_Ofp.png", AutoOfp)
+ggsave("Visual/Correlation_Ofn.png", AutoOfn)
+
+ggsave("Visual/Correlation_Cs.png", AutoCs)
+ggsave("Visual/Correlation_Cg.png", AutoCg)
+ggsave("Visual/Correlation_Cfp.png", AutoCfp)
+ggsave("Visual/Correlation_Cfn.png", AutoCfn)
--- Analysis/Climwin/Correlations/required_functions.R
+++ Analysis/Climwin/Correlations/required_functions.R
@@ -1,5 +1,19 @@
-autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
+cor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner, Only_Auto = FALSE, plot = TRUE){
   
+  data <- corr_climate(species, vitalrate, Climate, SlidingObject, Winner, Only_Auto)
+  
+  if (plot == FALSE){
+    return(data)
+  } else {
+    
+    data$correlations <- tidyr::pivot_longer(data$correlations, -c(Open, Close), names_to = "Clim_variable", values_to = "corr")
+    plot <- corplot(data)
+    return(plot)
+  }
+}
+
+
+corr_climate <- function(species, vitalrate, Climate, SlidingObject, Winner, Only_Auto = FALSE){
   enquo(Climate)
 
   options <- SlidingObject$combos[Winner,]
@@ -9,13 +23,18 @@ autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
 
   #### Set Range ----------------------------------------------------------------------------------------------------------------------------
   if(vitalrate == "s") {
+    if(species == "FRSP") {
+      print("Range set to 6 years")
+      range <- c(60, -12)
+    } else {
     range <- c(24,-12)
+    }
   }
   
   if(vitalrate == "g") {
     if(species == "FRSP") {
-      print("Range set to 5 years")
-      range <- c(48, -12)
+      print("Range set to 6 years")
+      range <- c(60, -12)
     } else {
       range <- c(24,-12)
     }
@@ -40,6 +59,8 @@ autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
   }
 
   #### End data fromattin 
+  
+  
   if(species == "FRSP" & vitalrate == "fn"){
     reference <- SlidingObject[[Winner]]$BestModelData %>%
       select(yearT1, climate) %>%
@@ -49,6 +70,8 @@ autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
     select(year, climate) %>%
     unique() 
   }
+  
+  
   reference$date <- as.Date(paste(SlidingObject[[Winner]]$Dataset$Reference.day[1], "/",
                                   SlidingObject[[Winner]]$Dataset$Reference.month[1], "/",
                                   reference$year,
@@ -56,8 +79,15 @@ autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
                             format = "%d/%m/%Y")
   reference <- reference[order(reference$year),]
   
-  Clim <- Clim[,c("date", options$climate)] %>%
+
+  if (Only_Auto == FALSE){
+  Clim <- Clim %>%
+    select(sum_prcp, mean_tavg, mean_tmin, mean_tmax, SPEI, date) %>%
     mutate(date = as.Date(date, format = "%d/%m/%Y"))
+  } else {
+    Clim <- Clim %>%
+      select(options$climate, date )
+  }
   
   BestWindowOpen <- SlidingObject[[Winner]]$Dataset$WindowOpen[1]
   BestWindowClose <- SlidingObject[[Winner]]$Dataset$WindowClose[1]
@@ -68,7 +98,7 @@ autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
   windows <- data.frame(Open = SlidingObject[[Winner]]$Dataset$WindowOpen,
                         Close = SlidingObject[[Winner]]$Dataset$WindowClose)
   
-  windows$corr <- NA
+for (x in names(Clim[c(1:length(names(Clim))-1)])) {
   
   for (i in c(1:length(windows$Open))){
     
@@ -77,33 +107,35 @@ autocor_wrap <- function(species, vitalrate, Climate, SlidingObject, Winner){
     for(j in c(1:length(reference$date))){
       
       seqDate <- seq.Date(from = reference$date[j] %m+% months(-windows$Open[i]),
-                          to = reference$date[j] %m+% months(-windows$Close[i]),
-                          by = "month")
+                          to = reference$date[j] %m+% months(-(windows$Close[i]-1)),
+                          by = "day")
       ClimMat[j] <- do.call(options$stat,
-                            list(Clim[which(lubridate::month(Clim$date) %in% lubridate::month(seqDate) &
-                                            lubridate::year(Clim$date) %in% lubridate::year(seqDate)),
-                                      options$climate],
+                            list(Clim[which(as.Date(Clim$date, format = "%d/%m/%Y") %in% seqDate),
+                                      x],
                                  na.rm = T))
       
     }
     
-    windows$corr[i] <- cor(reference$climate, ClimMat, use = "complete.obs")
+    windows[i, x] <- cor(reference$climate, ClimMat, use = "complete.obs")
     
   }
+}
   windows <- list(correlations = windows,
-                  df = data.frame(BestWindowOpen = BestWindowOpen,
+                  df = data.frame(Clim_variable = as.character(options$climate),
+                                  BestWindowOpen = BestWindowOpen,
                                   BestWindowClose = BestWindowClose)
   )
+  
   return(windows) 
 }  
 
 
-
-acorplot <- function(cor.output) {
-
+corplot <- function(cor.output) {
+  
   plot <- ggplot() + 
         geom_tile(data = cor.output$correlations, aes(x = Close, y = Open, fill = corr)) + 
-        geom_point(data = cor.output$df, aes(x = BestWindowClose, y = BestWindowOpen, z = NULL), size = 5, shape = 1, stroke = 2) +
+        geom_point(data = cor.output$df, aes(x = BestWindowClose, y = BestWindowOpen, z = NULL), 
+                   size = 5, shape = 1, stroke = 2) +
         scale_fill_gradient2(low = "red", mid = "yellow", high = "blue", 
                              midpoint = 0, limit = c(-1,1), name = "") + 
         theme_classic() + 
@@ -111,9 +143,11 @@ acorplot <- function(cor.output) {
               panel.grid.minor = element_blank(), 
               axis.line = element_line(size = 0.25, colour = "black"), 
               plot.title = element_text(size = 16, hjust = 0.5)) + 
-        ggtitle("Autocorrelation") + 
+    ggtitle(paste("Correlation between the mean", cor.output$df$Clim_variable, "from \nmonth", cor.output$df$BestWindowOpen, "to month", cor.output$df$BestWindowClose,
+                  "with all climate variables", sep = " ")) +
         ylab("Window open") + 
-        xlab("Window close")
+        xlab("Window close") + 
+    facet_wrap(vars(Clim_variable))
   
   return(plot)
   
