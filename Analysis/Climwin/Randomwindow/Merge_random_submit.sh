a8e86981684cd4a75714f82d747ac1cda059019a
Sanne Evers <sanne.evers@idiv.de>
2020-04-16 10:17:29 +0200

    updated random merge script according to suggestions shellcheck

--- Analysis/Climwin/Randomwindow/Merge_random_submit.sh
+++ Analysis/Climwin/Randomwindow/Merge_random_submit.sh
@@ -25,5 +25,5 @@ module load foss/2018b R/3.5.1
 ## The master submit will pass the output directory that belongs to the first job to output
 output_dir=$1
 
-Rscript $HOME/Biome/Analysis/Climwin/Randomwindow/Merge_random.R \
-  $output_dir
+Rscript "$HOME"/Biome/Analysis/Climwin/Randomwindow/Merge_random.R \
+  "$output_dir"
