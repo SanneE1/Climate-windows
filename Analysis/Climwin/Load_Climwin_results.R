7b7cb841b0f7e3aee3bd8c51f3f850fa2b52d7a3
Sanne <sanne.evers@idiv.de>
2020-05-05 20:17:23 +0200

    corrected results loading

--- Analysis/Climwin/Load_Climwin_results.R
+++ Analysis/Climwin/Load_Climwin_results.R
@@ -24,7 +24,7 @@ Cfn <- readRDS("Results/Climwin/CRFL_fn_month_result.rds")
 
 Csr <- readRDS("Results/Climwin/CRFL_s_month_random.rds")
 Cgr <- readRDS("Results/Climwin/CRFL_g_month_random.rds")
-Cfpr <- readRDS("Results/Climwin/CRFL_fp_month_random4.rds")
+Cfpr <- readRDS("Results/Climwin/CRFL_fp_month_random.rds")
 Cfnr <- readRDS("Results/Climwin/CRFL_fn_month_random.rds")
 
 ### OPIM
@@ -40,32 +40,33 @@ Ofnr <- readRDS("Results/Climwin/OPIM_fn_month_random.rds")
 
 ### FRSP
 Fs <- readRDS("Results/Climwin/FRSP_s_month_result.rds")
-Fg <- readRDS("Results/Climwin/FRSP_g_month_result_5yrs.rds")
+Fg <- readRDS("Results/Climwin/FRSP_g_month_result.rds")
 Ffp <- readRDS("Results/Climwin/FRSP_fp_month_result.rds")
 Ffn <- readRDS("Results/Climwin/FRSP_fn_month_result.rds")
 
 Fsr <- readRDS("Results/Climwin/FRSP_s_month_random.rds")
-Fgr <- readRDS("Results/Climwin/FRSP_g_month_random_5yrs.rds")
+Fgr <- readRDS("Results/Climwin/FRSP_g_month_random.rds")
 Ffpr <- readRDS("Results/Climwin/FRSP_fp_month_random.rds")
 Ffnr <- readRDS("Results/Climwin/FRSP_fn_month_random.rds")
 
 ### Set winners ---------------------------------------------------------------------------------------------------------------------------------------------
-Hsurv <- 13
-Hgrowth <- 2
-HpFlwr <- 16
-HnFlwr <-12 
 
-Fsurv <- 14
-Fgrowth <- 10
-FpFlwr <- 14
-FnFlwr <- 2
+Hsurv <- 9
+Hgrowth <- 9
+HpFlwr <- 3
+HnFlwr <-7
 
-Csurv <- 16
+Fsurv <- 9
+Fgrowth <- 7
+FpFlwr <- 3
+FnFlwr <- 10
+
+Csurv <- 3
 Cgrowth <- 1
-CpFlwr <- 4
-CnFlwr <- 4
+CpFlwr <- 2
+CnFlwr <- 2
 
-Osurv <- 3
-Ogrowth <- 12
-OpFlwr <- 2
-OnFlwr <- 3
+Osurv <- 10
+Ogrowth <- 10
+OpFlwr <- 9
+OnFlwr <- 10
