86b23be18484f08f42633ba85a2cb21c79746d65
Sanne <sanne.evers@idiv.de>
2020-11-10 13:33:05 +0100

    add randomization for HEQU spatial analysis

---
+++ Analysis/Climwin/Randomwindow/HEQU_spatial_random.R
@@ -0,0 +1,153 @@
+## run the randomization analysis of selected climate driver
+
+suppressPackageStartupMessages(library(climwin))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(lme4))
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(lubridate))
+
+
+#  ----------------------------------------------------------------------------------------------------------------------------
+# parsing arguments
+#  ----------------------------------------------------------------------------------------------------------------------------
+
+parser <- OptionParser(
+  usage       = "Rscript %prog [options] Climate SpeciesInput Results_sliding winner output",
+  description = "\nan Run randomwindow analysis",
+  epilogue    = ""
+)
+
+cli <- parse_args(parser, positional_arguments = 5)
+
+### Assign shortcuts ------------------------------------------------------------------------------------------
+
+Climate   <- cli$args[1]
+SpeciesInput  <- cli$args[2]
+Results_sliding <- cli$args[3]
+output <- cli$args[5]
+taskID <- as.integer(Sys.getenv("SGE_TASK_ID"))
+
+
+## Get which vital rate was analysed ------------------------------------------------------------------------------
+getinfo <- stringr::str_split(Results_sliding, "[[:punct:]]")
+vitalrate <- getinfo[[1]][7]
+
+
+vitalrate
+Climate
+SpeciesInput
+Results_sliding
+
+
+##----------------------------------------------------------------------------------------------------------------------------------
+## Prepare Climate data 
+##----------------------------------------------------------------------------------------------------------------------------------
+
+Clim <- read.csv(Climate)                                                ### get a date that's accepted by climwin
+Clim$date <- paste("15/",sprintf("%02d", Clim$Month), "/", Clim$Year, sep = "")          
+
+
+##----------------------------------------------------------------------------------------------------------------------------------
+## Prepare Biological data 
+##----------------------------------------------------------------------------------------------------------------------------------
+
+### Species specific data modification
+
+Biol <- read.csv(SpeciesInput) %>%
+  mutate(sizeT = as.integer(sizeT),
+         sizeT1 = as.integer(sizeT1))
+Biol <- Biol[which(Biol$seedling != 1),] # filter out seedlings - seperate stage class in previous lit.                          
+Biol <- Biol[which(Biol$year!= 2012),] # filter out transition 2012-2013 due to gophers
+
+Biol <- Biol[which(!(is.na(Biol$sizeT) | Biol$sizeT == 0)),] # filter out individuals without size in time t
+Biol$lnsizeT <- log(Biol$sizeT) # log transform size at time t
+
+Biol$date <- paste(ifelse(!(is.na(Biol$day)), sprintf("%02d", Biol$day), "01") , sprintf("%02d", Biol$month), Biol$year, sep = "/")                  ### get a date that's accepted by climwin
+
+##----------------------------------------------------------------------------------------------------------------------------------
+## Specify species and vital rate specific baseline 
+##----------------------------------------------------------------------------------------------------------------------------------
+
+if (vitalrate == "s") {                  
+  print("Running survival vital rate")
+  Biol <- Biol[which(!(is.na(Biol$survival))),]
+  model <- glmer(formula = survival ~ lnsizeT + population + (1|year),
+                 data = Biol, 
+                 family = binomial) 
+}
+
+if (vitalrate =="g"){             
+  print("Running growth vital rate")
+  Biol <- Biol[which(!is.na(Biol$sizeT1)),]
+  model <- glmer(sizeT1 ~ lnsizeT + population + (1|year),
+                 data = Biol,
+                 family = poisson)                          
+}
+
+if (vitalrate =="fp"){
+  print("Running Flower probability vital rate")
+  Biol <- Biol[which(!(is.na(Biol$pflowerT))),]
+  model <- glmer(formula = pflowerT ~ lnsizeT + population + (1|year),
+                 data = Biol,
+                 family = binomial)
+}
+
+if (vitalrate =="fn") {                        
+  print("Running Number of Flowers")
+  Biol <- Biol[which(!is.na(Biol$fertilityT)),]
+  Biol$fertilityT <- as.integer(Biol$fertilityT)
+  model <- glmer(formula = fertilityT ~ lnsizeT + population + (1|year),
+                 data = Biol,
+                 family = poisson)
+}
+
+
+#### Set Range ----------------------------------------------------------------------------------------------------------------------------
+
+if(vitalrate == "s") {
+  print("Range set to 3 years")
+  range <- c(24,-12)
+}
+
+if(vitalrate == "g") {
+    print("Range set to 3 years")
+    range <- c(24,-12)
+}
+
+if(vitalrate == "fp") {
+  print("Range set to 3 years")
+  range <- c(36, 0)
+}
+
+if(vitalrate == "fn") {
+  print("Range set to 3 years")
+  range <- c(36, 0)
+}
+
+
+##----------------------------------------------------------------------------------------------------------------------------------
+## Randomized run for selected combination
+##----------------------------------------------------------------------------------------------------------------------------------
+
+str(x)
+
+random <- randwin(repeats = 10,
+                  baseline =  model   ,
+                  xvar = list(Temp = Clim$tmean_scaled,
+                              Ppt = Clim$ppt_scaled),
+                  type = "absolute",
+                  range = range,
+                  stat = c(as.character(results$combos$stat[w])),
+                  func = c(as.character(results$combos$func[w])),
+                  refday = c(as.integer(format(min(as.Date(Biol$date, format = "%d/%m/%Y")), format = "%d")), as.integer(format(min(as.Date(Biol$date, format = "%d/%m/%Y")), format = "%m"))),                                                          
+                  cinterval = "month",
+                  cdate = Clim$date, bdate = Biol$date,
+                  window = "sliding",
+                  cmissing = "method1",
+                  spatial = list(as.factor(Biol$population), as.factor(Clim$population))
+                  )
+
+
+
+saveRDS(random, file = output)
+
---
+++ Analysis/Climwin/Randomwindow/HEQU_spatial_submit.R
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+
+#$ -wd /work/$USER
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID-$TASK_ID.log
+#$ -j y
+
+#Specify job name
+#$ -N Random
+
+#Resources
+# max running time
+
+# memory per core (hard limit)
+#$ -l h_vmem=8G
+
+# Array numbers 
+#$ -t 1-200
+
+#needed when submitting a non-parallel job
+#$ -binding linear:1
+
+#create a single output directory per job
+output_dir="/work/$USER/$JOB_NAME-$JOB_ID"
+mkdir -p "$output_dir"
+
+module load foss/2018b R/3.5.1
+
+Climate=$1
+SpeciesInput=$2
+Results_sliding=$3
+vitalrate=$(basename "$Results_sliding" .rds | cut -d _ -f2)
+output="$output_dir"/${JOB_NAME}_month_${vitalrate}_${JOB_ID}_$SGE_TASK_ID.rds
+
+
+Rscript "$HOME"/Biome/Analysis/Climwin/Randomwindow/Random.R \
+"$Climate" \
+"$SpeciesInput" \
+"$Results_sliding" \
+"$winner" \
+"$output"
